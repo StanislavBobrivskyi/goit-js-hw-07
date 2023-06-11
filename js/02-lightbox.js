37b9e45f872f3ae0b74f5aeb21680cb27a1ee728
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-06-11 18:05:44 +0200

    task - 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-import * as basicLightbox from "basiclightbox";
-
-const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`);
-
-instance.show();
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const createCard = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`
+);
+
+galleryEl.insertAdjacentHTML("beforeend", createCard.join(""));
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
