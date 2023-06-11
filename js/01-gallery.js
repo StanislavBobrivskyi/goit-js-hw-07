9e9498cd7c61d99cf53ceda91e50cd35583b1fac
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-06-11 13:21:21 +0200

    1 - task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
+const galleryEl = document.querySelector(".gallery");
+
 console.log(galleryItems);
+
+const createCard = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+    </a>
+    </li>`
+);
+
+galleryEl.insertAdjacentHTML("beforeend", createCard.join(""));
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}" width="600" height="400">
+`);
+  //   window.close(escape);
+  instance.show();
+}
